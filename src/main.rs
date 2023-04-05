ee6fa9d734cb1566ad8f015339ce02921afa4cd5
root <mcnabb.patrick@yahoo.com>
2023-04-05 21:25:28 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+    let string_borrow: &str = &string;   // string slice pointing to start of string
+    let string_literal = "1234";
+
+    dbg!(&string);
+    dbg!(string_slice);
+    //let server = Server::new("127.0.0.1:8080");
+    //server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+
+    }
+}
