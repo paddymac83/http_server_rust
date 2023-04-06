7d1b510f4e8fd6e3e70a7d672dec856aea5cb0ba
root <mcnabb.patrick@yahoo.com>
2023-04-06 14:55:25 +0100

    Added modules for http and server functions

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,13 @@
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
+
 fn main() {
 
     let server = Server::new("127.0.0.1:8080".to_string());   // convert literal to String type
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr)
-
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: String,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
